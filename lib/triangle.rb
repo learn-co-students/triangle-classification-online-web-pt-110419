16180e60f5c0e5fd460afc3b5b0f5d09426651de
Anthony Mai <anthony.mai530@gmail.com>
2019-12-30 03:10:51 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,15 +1,15 @@
 class Triangle
-  def initialize(side_1, side_2, side_3)
+  def initialize(length1, length2, length3)
     @triangle_sides = []
-    @triangle_sides << side_1
-    @triangle_sides << side_2
-    @triangle_sides << side_3
+    @triangle_sides << length1
+    @triangle_sides << length2
+    @triangle_sides << length3
   end
 
-  def valid?
+  def triangle?
     sum_one_two = @triangle_sides[0] + @triangle_sides[1]
-    sum_one_three = @triangle_sides[0] + @triangle_sides[2]
     sum_two_three = @triangle_sides[1] + @triangle_sides[2]
+    sum_one_three = @triangle_sides[0] + @triangle_sides[2]
 
     if (@triangle_sides.none? {|side| side <= 0}) &&
       (sum_one_two > @triangle_sides[2] && sum_one_three > @triangle_sides[1] && sum_two_three > @triangle_sides[0])
@@ -20,7 +20,7 @@ class Triangle
   end
 
   def kind
-    if valid?
+    if triangle?
       if @triangle_sides.uniq.length == 1
         return :equilateral
       elsif @triangle_sides.uniq.length == 2
