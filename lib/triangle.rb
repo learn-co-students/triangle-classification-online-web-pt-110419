edd885a47cea3d74bec0aa44e6614291786320ba
Ronald Taylor <ronald.taylorjr1986@gmail.com>
2020-01-06 22:09:13 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,11 @@
 class Triangle
-  # write code here
+  attr_accessor :equilateral, :isosceles, :scalene
 end
+
+def kind(TriangleError)
+
+class TriangleError < StandardError
+  def message
+    "the sum of the lengths of a triangle always exceeds the length of the third side."
+    
+  
