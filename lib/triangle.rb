58e24b3516920aea03f8bce9fba2ee38f08fe1b2
Zitai Huang <mywpiedu@gmail.com>
2020-01-13 02:19:07 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,44 @@
 class Triangle
   # write code here
+  attr_accessor :len1, :len2, :len3 , :allin
+  def initialize(len1,len2,len3)
+    @len1 = len1 
+    @len2 = len2 
+    @len3 = len3
+    @allin = [len1,len2,len3]
+  end 
+  
+  
+  def kind 
+    sum = @len1 +@len2 + @len3 
+    if @allin.all? {|a| a >0} == false  
+      raise TriangleError
+       
+       
+    elsif @allin.all? {|a| a*2 < sum} == false 
+       raise TriangleError 
+    
+    
+    
+    end 
+    
+    
+    
+    if  @len1 ==@len2 && @len2 == @len3 
+      return :equilateral
+    end 
+    
+    if @allin == @allin.uniq
+      return :scalene
+    end 
+    
+      return :isosceles
+    
+     
+  end 
+  
+  
+  class TriangleError < StandardError
+    
+  end 
 end
